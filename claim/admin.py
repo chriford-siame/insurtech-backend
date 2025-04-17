3acb3ed2f2dea77882954e2ea3e3d4859fc45481
chriford-siame <chrifordsiame@gmail.com>
2025-04-17 22:34:02 +0200

    register models

--- claim/admin.py
+++ claim/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from claim.models import Claimant, Reviewer, ClaimFile
+
+admin.site.register(Claimant)
+admin.site.register(Reviewer)
+admin.site.register(ClaimFile)
 
 # Register your models here.
