177f46076668ca4a4fd55b2316ffd2d6a6f5ad20
chriford-siame <chrifordsiame@gmail.com>
2025-04-17 22:36:23 +0200

    register endpoints

--- claim/routers.py
+++ claim/routers.py
@@ -1,11 +1,16 @@
 from rest_framework.routers import DefaultRouter
 
 from claim.viewsets import (
-    UserViewSet    
+    UserViewSet    ,
+    ClaimantViewSet,
+    ReviewerViewSet
 )
 
 router = DefaultRouter()
+
 router.register(r'users', UserViewSet)
+router.register(r'claimants', ClaimantViewSet, basename='claimant')
+router.register(r'reviewers', ReviewerViewSet, basename='reviewer')
 
 app_name = 'claim'
 urlpatterns = router.urls
