ea6a2e878bedbe0e91383c3d00c40a614b1d82eb
chriford-siame <chrifordsiame@gmail.com>
2025-04-17 22:36:05 +0200

    create serializers

--- claim/serializers/serializers.py
+++ claim/serializers/serializers.py
@@ -1,9 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
-# from claim.models import (
-    
-# )
+from claim.models import Claimant, Reviewer, ClaimFile
 
 class UserSerializer(serializers.ModelSerializer):
     """
@@ -13,3 +11,24 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+
+class ClaimFileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ClaimFile
+        fields = ['id', 'file']
+
+
+class ClaimantSerializer(serializers.ModelSerializer):
+    files = ClaimFileSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Claimant
+        fields = '__all__'
+
+
+class ReviewerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reviewer
+        fields = '__all__'
+
