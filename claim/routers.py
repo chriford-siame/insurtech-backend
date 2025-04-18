428108876c8e814dc441936fd0ba29cbc0b1948b
chriford-siame <chrifordsiame@gmail.com>
2025-04-18 12:26:55 +0200

    register viewset

--- claim/routers.py
+++ claim/routers.py
@@ -3,7 +3,8 @@ from rest_framework.routers import DefaultRouter
 from claim.viewsets import (
     UserViewSet    ,
     ClaimantViewSet,
-    ReviewerViewSet
+    ReviewerViewSet,
+    ClaimFileViewSet,
 )
 
 router = DefaultRouter()
@@ -11,6 +12,7 @@ router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'claims', ClaimantViewSet, basename='claims')
 router.register(r'reviewers', ReviewerViewSet, basename='reviewer')
+router.register(r'claimfile', ClaimFileViewSet, basename='claimfile')
 
 app_name = 'claim'
 urlpatterns = router.urls
