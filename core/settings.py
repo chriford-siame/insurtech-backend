de897426b5080ba9a74714f3d9fb5c8b7f6ef819
chriford-siame <chrifordsiame@gmail.com>
2025-04-18 10:26:04 +0200

    Add CORS configurations

--- core/settings.py
+++ core/settings.py
@@ -68,6 +68,7 @@ SWAGGER_SETTINGS = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # must be high up
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -162,7 +163,7 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'core/media/'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # React.js local development server
+    "http://localhost:3000" # React.js local development server
 ]
 
 # Looking to send emails in production? Check out our Email API/SMTP product!
