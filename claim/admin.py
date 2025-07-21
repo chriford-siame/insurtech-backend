3a817c4a675ea29d6128a994edbc1b0e888cefcd
chriford-siame <chrifordsiame@gmail.com>
2025-07-21 16:40:27 +0200

    register tables

--- claim/admin.py
+++ claim/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from claim.models import Claimant, Reviewer, ClaimFile
+from claim.models import (
+    Claimant, 
+    Reviewer, 
+    ClaimFile,
+    MakeYear,
+    Make,
+    Model,
+    Quotation,
+)
 
 admin.site.register(Claimant)
 admin.site.register(Reviewer)
 admin.site.register(ClaimFile)
+admin.site.register(MakeYear)
+admin.site.register(Make)
+admin.site.register(Model)
+admin.site.register(Quotation)
 
 # Register your models here.
