9c7e0ed6882322e16a7d375490769499fb7597a6
chriford-siame <chrifordsiame@gmail.com>
2025-07-21 16:40:34 +0200

    refactor routes

--- claim/routers.py
+++ claim/routers.py
@@ -5,6 +5,10 @@ from claim.viewsets import (
     ClaimantViewSet,
     ReviewerViewSet,
     ClaimFileViewSet,
+    MakeYearViewSet,
+    MakeViewSet,
+    ModelViewSet,
+    QuotationViewSet,
 )
 
 router = DefaultRouter()
@@ -13,6 +17,10 @@ router.register(r'users', UserViewSet)
 router.register(r'claims', ClaimantViewSet, basename='claims')
 router.register(r'reviewers', ReviewerViewSet, basename='reviewer')
 router.register(r'claimfiles', ClaimFileViewSet, basename='claimfiles')
+router.register(r'make', MakeViewSet, basename='make')
+router.register(r'make-year', MakeYearViewSet, basename='make-year')
+router.register(r'make-model', ModelViewSet, basename='make-model')
+router.register(r'quotation', QuotationViewSet, basename='quotation')
 
 app_name = 'claim'
 urlpatterns = router.urls
