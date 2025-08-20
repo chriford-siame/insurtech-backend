4fd006aee61d1363619cf5bfbcd14a514ae61c30
Siame <chrifordsiame@gmail.com>
2025-08-21 00:18:15 +0100

    Hash password

--- claim/serializers/serializers.py
+++ claim/serializers/serializers.py
@@ -12,14 +12,22 @@ from claim.models import (
 )
 
 class UserSerializer(serializers.ModelSerializer):
-    """
-    Serializer for User model.
-    """
-
     class Meta:
         model = User
         fields = '__all__'
-        
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
 class MakeYearSerializer(serializers.ModelSerializer):
     """
     Serializer for MakeYear model.
